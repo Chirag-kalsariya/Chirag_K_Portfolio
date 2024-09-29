fe6bc9d5188011efdbf65329309ccdf9238b988a
Charles <chiragkalsariya05@gmail.com>
2024-09-28 19:02:55 -0500

    Init project

---
+++ pages/_meta.tsx
@@ -0,0 +1,17 @@
+const meta = {
+  index: "Introduction",
+  another: "Another Page",
+  advanced: "Advanced (A Folder)",
+  about: {
+    title: "About",
+    type: "page"
+  },
+  contact: {
+    title: "Contact ↗",
+    type: "page",
+    href: "https://twitter.com/shuding_",
+    newWindow: true
+  }
+};
+
+export default meta;
