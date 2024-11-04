cb707f10497907b073a946fcd8115616307bee88
Charles <chiragkalsariya05@gmail.com>
2024-11-04 14:17:12 -0600

    Bug fix

---
+++ src/custom/linespacer/linespacer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const LineSpaceBox: React.FC = () => {
+    return (
+        <div
+            style={{
+                paddingTop: "3rem",
+                paddingBottom: "1rem",
+            }}
+        >
+            <div
+                style={{
+                    borderColor: "#e5e5e5b3",
+                    borderBottomWidth: "1px",
+                    borderBottomStyle: "solid",
+                }}
+            ></div>
+        </div>
+    );
+};
