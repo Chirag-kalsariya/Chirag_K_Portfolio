76d07de75180ce2ceca2925b2b27135ad02fb998
Chirag kalsariya <chiragkalsariya05@gmail.com>
2025-10-26 14:28:55 -0500

    Cleaning

--- pages/_meta.tsx
+++
@@ -1,9 +0,0 @@
-const meta = {
-  index: "Introduction",
-  technicalskills: "Technical Skills",
-  experience: "Experience",
-  projects: "Projects",
-  contributions: "Contributions",
-};
-
-export default meta;
