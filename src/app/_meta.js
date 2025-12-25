c71f841c4b76c711e9595e8b1c3e417624e2f244
Chirag kalsariya <chiragkalsariya05@gmail.com>
2025-12-24 18:07:32 -0600

    Setup init

---
+++ src/app/_meta.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    index: 'Introduction',
+    technicalskills: 'Technical Skills',
+    experience: 'Experience',
+    projects: 'Projects',
+    contributions: 'Contributions'
+};
